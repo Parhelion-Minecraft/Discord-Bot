eab4b257b153cb3642bee6d24e9b0765ae490667
LProgead <lprogead@mailo.com>
2021-07-16 18:41:48 +0200

    Change identation in index.js

--- index.js
+++ index.js
@@ -9,24 +9,24 @@ const Enmap = require('enmap');
 client.login(process.env.token);
 
 fs.readdir("./events/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    console.log(`Chargement en cours de l'événement ${eventName}.`);
-    client.on(eventName, event.bind(null, client));
-  });
+	if (err) return console.error(err);
+	files.forEach(file => {
+		const event = require(`./events/${file}`);
+		let eventName = file.split(".")[0];
+		console.log(`Chargement en cours de l'événement ${eventName}.`);
+		client.on(eventName, event.bind(null, client));
+	});
 });
 
 client.commands = new Enmap();
 
 fs.readdir("./commands/", (err, files) => {
-  if (err) return console.error(err);
-  files.forEach(file => {
-    if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
-    let commandName = file.split(".")[0];
-    console.log(`Chargement en cours de la commande ${commandName}.`);
-    client.commands.set(commandName, props);
-  });
+	if (err) return console.error(err);
+	files.forEach(file => {
+		if (!file.endsWith(".js")) return;
+		let props = require(`./commands/${file}`);
+		let commandName = file.split(".")[0];
+		console.log(`Chargement en cours de la commande ${commandName}.`);
+		client.commands.set(commandName, props);
+	});
 });
