7e3d7cf3e484df2fd92a46d1978ff316c604208b
LProgead <lprogead@mailo.com>
2021-07-16 12:41:23 +0200

    Adding autorole

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,16 @@
+module.exports = (client, member) => {
+    const config = require('../config.json');
+    const { MessageEmbed } = require('discord.js');
+
+    const greating_embed = new MessageEmbed()
+        .setAuthor("Parhelion", member.guild.iconURL())
+        .setTitle("Bienvenue !")
+        .setDescription(`<@${member.user.id}> vient de rejoindre **Parhelion Minecraft** ! \nAccueilliez-le comme il se doit !`)
+        .setThumbnail(member.user.displayAvatarURL())
+
+    client.channels.cache.get(config.greatings_channel).send(greating_embed);
+
+    member.guild.members.cache.get(member.user.id).roles.add(config.member_role);
+}
+
+//dUA6hZK4
