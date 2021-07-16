b30abf0d19d5e82519216b1a227a48c73c578171
LProgead <lprogead@mailo.com>
2021-07-16 11:57:40 +0200

    Initial commit

--- source/essentials/config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-  PREFIX: "pm.",
-};
-
-module.exports = config;
--- source/events/ready.js
+++
@@ -1,5 +0,0 @@
-const { PREFIX } = require("../essentials/config.js");
-
-module.exports = async (bot, data) => {
-  bot.user.setActivity(`➜ ${PREFIX}help`);
-};
