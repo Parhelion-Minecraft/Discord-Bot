a41196b030193351e8b601bbe4880d78fed3d4b6
LProgead <lprogead@mailo.com>
2021-07-17 18:15:52 +0200

    Add permissions to user who opens a ticket

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -5,28 +5,31 @@ module.exports = (client, reaction, user) => {
 
     if (reaction.message.channel.id === config.ticket_channel) {
         reaction.users.remove(user.id);
-        
-        if (client.guilds.cache.get(config.server_id).channels.cache.filter(channel => channel.name === user.id).first()) {         
+
+        if (client.guilds.cache.get(config.server_id).channels.cache.filter(channel => channel.name === user.id).first()) {
             return user.send("Vous ne pouvez ouvrir qu'un seul ticket à la fois.");
         }
 
         client.guilds.cache.get(config.server_id).channels.create(user.id, { parent: config.ticket_category })
             .then(channel => {
-                channel.send(`Ticket créé par <@${user.id}>`);
+                channel.overwritePermissions([{ id: user.id, alow: ["VIEW_CHANNEL", "SEND_MESSAGES"] }])
+                    .then(() => {
+                        channel.send(`Ticket créé par <@${user.id}>`);
 
-                switch (reaction.emoji.name) {
-                    case "❓":
-                        channel.send("Ce ticket concerne une demande d'aide.");
-                    break;
+                        switch (reaction.emoji.name) {
+                            case "❓":
+                                channel.send("Ce ticket concerne une demande d'aide.");
+                                break;
 
-                    case "🔗":
-                        channel.send("Ce ticket concerne une demande de partenariat.");
-                    break;
+                            case "🔗":
+                                channel.send("Ce ticket concerne une demande de partenariat.");
+                                break;
 
-                    case "📯":
-                        channel.send("Ce ticket concerne une catégorie non listée.");
-                    break;
-                }
+                            case "📯":
+                                channel.send("Ce ticket concerne une catégorie non listée.");
+                                break;
+                        }
+                    });
             });
     }
 }
