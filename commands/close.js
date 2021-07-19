a48cabbdd47f4b96af0433f6f6112ad6614bfc02
LProgead <lprogead@mailo.com>
2021-07-19 16:09:08 +0200

    Fix close bug

--- commands/close.js
+++ commands/close.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args) => {
     const config = require('../config.json');
 
-    if (message.channel.parentID === config.ticket_category) {
+    if (message.channel.parentId === config.ticket_category) {
         message.channel.delete();
 
         client.users.cache.get(message.channel.name).send("Votre ticket de support a été fermé.");
