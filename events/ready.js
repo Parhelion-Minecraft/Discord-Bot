267fe4aa7e960e9590f2fdf24bf5ac96d695e1a9
LProgead <lprogead@mailo.com>
2021-07-19 16:05:56 +0200

    Change ticket system to buttons

--- events/ready.js
+++ events/ready.js
@@ -4,27 +4,6 @@ module.exports = client => {
 
     console.log('Bot lancé avec succès !');
 
-    const ticket_embed = new MessageEmbed()
-        .setAuthor("Parhelion", client.guilds.cache.get(config.server_id).iconURL())
-        .setColor('#159997')
-        .setTitle('Contacter notre équipe')
-        .setDescription("Vous souhaitez contacter notre équipe ? Vous êtes au bon endroit ! \nTout d'abord, sur quoi porte votre demande ?")
-        .addField("Demander de l'aide", "Réagissez avec l'emoji ❓")
-        .addField("Proposer un partenariat", "Réagissez avec l'emoji 🔗")
-        .addField("Autre", "Réagissez avec l'emoji 📯")
-        .setTimestamp()
-
-    client.channels.cache.get(config.ticket_channel).send({ embeds: [ticket_embed ] })
-        .then(message => {
-            const emojis = ["❓", "🔗", "📯"];
-
-            emojis.forEach((element, i) => {
-                setTimeout(() => {
-                    message.react(element)
-                }, 200 * i);
-            });
-        });
-
     const tmi = require('tmi.js');
 
     const twitchClient = new tmi.Client({
