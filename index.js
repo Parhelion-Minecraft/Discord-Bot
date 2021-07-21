785a8837c630c6df71b8451dcb2fadd1a3d2bc36
LProgead <lprogead@mailo.com>
2021-07-21 18:55:29 +0200

    New members are now detected !

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_MEMBERS] });
 
 require('dotenv').config();
 
