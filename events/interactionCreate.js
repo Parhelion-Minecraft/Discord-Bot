cdad4c1d4215f756c9d647bb105f3dc78ad1dab2
LProgead <lprogead@mailo.com>
2021-08-01 12:14:32 +0200

    Prepare interaction event to use slash commands

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,21 +1,20 @@
 module.exports = (client, interaction) => {
     const { MessageEmbed } = require('discord.js');
 
-    if (interaction.isMessageComponent()) {
+    if (interaction.isCommand()) {
+        const commandName = interaction.commandName;
+
+        const command = client.commands.get(commandName);
+
+        if (!command) return;
+
+        command.run(client, interaction);
+    } else if (interaction.isMessageComponent()) {
         const customID = interaction.customId;
 
         const component = client.components.get(customID);
 
-        if (!component) {
-            const noexist_cmd = new MessageEmbed()
-                .setAuthor(message.author.username, message.author.displayAvatarURL())
-                .setColor('#FF2200')
-                .setTitle('Commande inexistante')
-                .setDescription('Vous avez tenté d\'exécuter une commande inexistante.')
-                .setTimestamp()
-
-            return interaction.reply({ embeds: [noexist_cmd], ephemeral: true });
-        }
+        if (!component) return;
 
         component.run(client, interaction);
     }
