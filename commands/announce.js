fa8636156ebaceaadb72d08ebad21faa16a2ae9c
LProgead <lprogead@mailo.com>
2021-08-01 12:14:51 +0200

    Announce command is now slash commands friendly

--- commands/announce.js
+++ commands/announce.js
@@ -1,33 +1,31 @@
-exports.run = (client, message, args) => {
+exports.run = (client, interaction) => {
     const config = require('../config.json');
     const { MessageEmbed, WebhookClient } = require('discord.js');
 
-    if (!message.member.roles.cache.has(config.admin_role)) {
+    if (!interaction.member.roles.cache.has(config.admin_role)) {
         const no_perm = new MessageEmbed()
-            .setAuthor("Parhelion", message.guild.iconURL())
+            .setAuthor("Parhelion", interaction.guild.iconURL())
             .setTitle("Erreur")
             .setColor("#FF2200")
             .setDescription(`Vous devez être administrateur pour pouvoir effectuer cette commande !`)
 
-        message.channel.send({ embeds: [no_perm] });
+        interaction.reply({ embeds: [no_perm] });
     } else {
         const hook = new WebhookClient(config.announcements_webhook["id"], config.announcements_webhook["token"]);
 
         hook.send({
             "embeds": [
                 {
-                    "description": message.content.replace('pm.announce', ""),
+                    "description": interaction.options.get('annonce').value,
                     "color": "#f57e2c"
                 }
             ]
         })
-            .then(() => {
-                message.delete();
-                
-                message.channel.send("Annonce publiée avec succès !");
+            .then(() => {                
+                interaction.reply("Annonce publiée avec succès !");
             })
             .catch(err => {
-                message.channel.send("Une erreur est survenue. Merci de consulter la console.");
+                interaction.reply("Une erreur est survenue. Merci de consulter la console.");
                 console.error(err);
             })
     }
