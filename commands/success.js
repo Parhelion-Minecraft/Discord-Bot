98f981fa02249b892dd5492b49801871550dcc1f
LProgead <lprogead@mailo.com>
2021-08-01 12:22:49 +0200

    Success command is now slash command friendly

--- commands/success.js
+++ commands/success.js
@@ -1,29 +1,15 @@
-exports.run = (client, message, args) => {
-    const config = require('../config.json');
-    const { MessageEmbed, WebhookClient } = require('discord.js');
+exports.run = (client, interaction) => {
+    const { MessageEmbed } = require('discord.js');
 
-    const text = message.content.replace("pm.success ", "");
-
-    if (text === "pm.success") {
-        const no_arg = new MessageEmbed()
-            .setAuthor("Parhelion", message.guild.iconURL())
+    if (interaction.options.get('annonce').value.length > 23) {
+        const too_long = new MessageEmbed()
+            .setAuthor("Parhelion", interaction.guild.iconURL())
             .setTitle("Erreur")
             .setColor("#FF2200")
-            .setDescription(`Vous devez indiquer le succès à générer.`)
+            .setDescription(`Votre succès doit contenir au maximum 23 caractères.`)
 
-        message.channel.send({ embeds: [no_arg] });
+        interaction.reply({ embeds: [too_long] });
     } else {
-        if (text.length > 23) {
-            const too_long = new MessageEmbed()
-                .setAuthor("Parhelion", message.guild.iconURL())
-                .setTitle("Erreur")
-                .setColor("#FF2200")
-                .setDescription(`Votre succès doit contenir au maximum 23 caractères.`)
-
-            message.channel.send({ embeds: [too_long] });
-        } else {
-            message.delete();
-            message.channel.send(`https://minecraftskinstealer.com/achievement/1/Succès%20débloqué%20!/${text.replace(/ /g, "%20")}`);
-        }
+        interaction.reply(`https://minecraftskinstealer.com/achievement/1/Succès%20débloqué%20!/${interaction.options.get('annonce').value}`);
     }
 }
