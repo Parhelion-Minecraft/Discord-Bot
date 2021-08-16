7a2f9f4eb3adf99a6f588c2f177fcce286eed959
LProgead <lprogead@mailo.com>
2021-08-16 15:20:02 +0200

    Reset invites when member leaves the guild

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,16 @@
+module.exports = (client, member) => {
+    const mysql = require('mysql');
+
+    const connection = new mysql.createConnection({
+        host: "lraspberrypi.zapto.org",
+        user: process.env.dbUsername,
+        password: process.env.dbPassword,
+        database: "parhelion"
+    });
+
+    connection.query(`SELECT * FROM invites WHERE inviter=${member.user.id}`, function (error, results, fields) {
+        if (!resultsh || !results[0]) return;
+
+        connection.query(`REMOVE FROM invites WHERE inviter=${member.user.id}`);
+    });
+}
