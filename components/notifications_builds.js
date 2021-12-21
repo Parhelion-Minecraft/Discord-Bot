81294accc9ac89074fc20432103efd8a3e135d03
LProgead <lprogead@mailo.com>
2021-12-21 17:58:14 +0100

    Build notification role fixed

--- components/notifications_builds.js
+++ components/notifications_builds.js
@@ -1,12 +1,12 @@
 exports.run = (client, interaction) => {
     const config = require('../config.json');
 
-    if (interaction.guild.members.cache.get(interaction.user.id).roles.cache.has(config.builds_roles)) {
-        interaction.guild.members.cache.get(interaction.user.id).roles.remove(config.builds_roles);
+    if (interaction.guild.members.cache.get(interaction.user.id).roles.cache.has(config.builds_role)) {
+        interaction.guild.members.cache.get(interaction.user.id).roles.remove(config.builds_role);
 
         interaction.reply({ content: "Rôle enlevé avec succès !", ephemeral: true });
     } else {
-        interaction.guild.members.cache.get(interaction.user.id).roles.add(config.builds_roles);
+        interaction.guild.members.cache.get(interaction.user.id).roles.add(config.builds_role);
 
         interaction.reply({ content: "Rôle ajouté avec succès !", ephemeral: true });
     }
